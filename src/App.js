22550d397380d28293af890d3008fd66473d1d0f
JEEL <Jeeldungarani7@gmail.com>
2021-11-27 22:52:19 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Permissions from './components/Permissions'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Permissions />
+    </div>
+  );
+}
+
+export default App;
